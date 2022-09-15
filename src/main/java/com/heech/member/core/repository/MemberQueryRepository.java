5463fb7ab8c4e44f12f343d1804dc3676c71c826
hclee <hclee@dataeum.kr>
2022-09-15 20:18:11 +0900

    core > member > repository, service 추가

---
+++ src/main/java/com/heech/member/core/repository/MemberQueryRepository.java
@@ -0,0 +1,18 @@
+package com.heech.member.core.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+public class MemberQueryRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    public MemberQueryRepository(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+
+}
---
+++ src/main/java/com/heech/member/core/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.heech.member.core.repository;
+
+import com.heech.member.core.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    /** 멤버 조회 by loginId */
+    Optional<Member> findByLoginId(String loginId);
+
+}
