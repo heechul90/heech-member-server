14e4d9b7909e8c2b36a85a6e1ba27edaa5ce303f
hclee <hclee@dataeum.kr>
2022-09-19 10:19:58 +0900

    common > jsonResult, jsonError 추가

--- src/main/java/com/heech/member/common/json/JsonError.java
+++ src/main/java/com/heech/member/common/json/JsonError.java
@@ -1,4 +1,14 @@
 package com.heech.member.common.json;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class JsonError {
+
+    private String fieldName;
+    private String errorMessage;
+
 }
