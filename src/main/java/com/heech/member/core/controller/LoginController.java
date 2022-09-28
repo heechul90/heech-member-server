6d0abddeeff5501dd9c347c8e4075a9e86abc204
hclee <hclee@dataeum.kr>
2022-09-28 21:49:33 +0900

    spring security 추가

--- src/main/java/com/heech/member/core/controller/LoginController.java
+++ src/main/java/com/heech/member/core/controller/LoginController.java
@@ -6,9 +6,10 @@ import com.heech.member.core.domain.Member;
 import com.heech.member.core.service.LoginService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.connector.Response;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
