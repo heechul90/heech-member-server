8462c65651ea099f85f5300aeffcf3e23ea9bb73
hclee <heechul4296@gmail.com>
2022-10-02 19:27:11 +0900

    core > join > controller > test 추가중

--- src/test/java/com/heech/member/core/controller/JoinControllerTest.java
+++ src/test/java/com/heech/member/core/controller/JoinControllerTest.java
@@ -1,10 +1,23 @@
 package com.heech.member.core.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.heech.member.core.service.JoinService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@WebMvcTest(JoinController.class)
 class JoinControllerTest {
+
+    @Autowired private MockMvc mockMvc;
+
+    @MockBean private JoinService joinService;
+
+    @Autowired private ObjectMapper objectMapper;
 
     @Test
     void joinMember() {
