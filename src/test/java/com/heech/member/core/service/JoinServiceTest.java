25d943de7dc64cd24ab21d8320a2975c79de56fc
hclee <hclee@dataeum.kr>
2022-10-07 18:43:35 +0900

    core > service > join > test 수정중

---
+++ src/test/java/com/heech/member/core/service/JoinServiceTest.java
@@ -0,0 +1,20 @@
+package com.heech.member.core.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class JoinServiceTest {
+
+    @Test
+    void joinMember() {
+        //given
+
+        //when
+
+        //then
+    }
+}
